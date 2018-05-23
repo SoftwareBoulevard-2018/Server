a00d55bf0a3bbb95448f5ab328c0d43d65f1f761
Novpic <juan-camilo-lm@hotmail.com>
2018-05-23 14:21:18 -0500

    added the sended to the index

--- services/email/index.js
+++ services/email/index.js
@@ -3,11 +3,13 @@ const express = require('express');
 const read = require('./read');
 const readOne = require('./readOne');
 const send = require('./send');
+const sended = require('./sended');
 
 const router = express.Router();
 
 router.get('/', read);
 router.post('/send', send);
 router.get('/:emailId', readOne);
+router.get('/sended',sended)
 
 module.exports = router;
