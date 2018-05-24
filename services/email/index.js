6cfc1ec4109ac29f3ba8de567eb7248c3eeeaf11
Juan Pablo Villegas Gómez <jupvillegasgo@unal.edu.co>
2018-05-23 23:19:38 -0500

    Fixed Listusers
    Fixed Listusers
    
    Co-Authored-By: Camilo <novpic@users.noreply.github.com>

--- services/email/index.js
+++ services/email/index.js
@@ -4,7 +4,7 @@ const read = require('./read');
 const readOne = require('./readOne');
 const send = require('./send');
 const sended = require('./sended');
-const listUsers = requiere('./listUsers');
+const listUsers = require('./listUsers');
 
 const router = express.Router();
 
