6ec4f60c4af1eec068541ab5ae71a0d31d45e792
Juan Chaves <jpchavesm@unal.edu.co>
2018-05-27 00:10:09 -0500

    fixed minor typo

--- services/biddingProject/index.js
+++ services/biddingProject/index.js
@@ -4,10 +4,10 @@ const router = express.Router();
 
 const createBiddingProject = require('./createBiddingProject');
 const updateBiddingProject = require('./updateBiddingProject');
-const getBiddingProjectbyId = require('./getBiddingProjectById');
+const getBiddingProjectById = require('./getBiddingProjectById');
 
 router.post('/createBiddingProject', createBiddingProject);
 router.put('/updateBiddingProject/:name', updateBiddingProject);
-router.get('/:companyId', getBiddingProjectbyId);
+router.get('/:companyId', getBiddingProjectById);
 
 module.exports = router;
