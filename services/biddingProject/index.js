0c0489629de9cfb38e8b86d11d6031e9ab33047e
Juan Chaves <jpchavesm@unal.edu.co>
2018-05-27 00:04:18 -0500

    Added a method to get bidding projects

--- services/biddingProject/index.js
+++ services/biddingProject/index.js
@@ -4,8 +4,10 @@ const router = express.Router();
 
 const createBiddingProject = require('./createBiddingProject');
 const updateBiddingProject = require('./updateBiddingProject');
+const getBiddingProjectbyId = require('./getBiddingProjectById');
 
 router.post('/createBiddingProject', createBiddingProject);
 router.put('/updateBiddingProject/:name', updateBiddingProject);
+router.get('/:companyId', getBiddingProjectbyId);
 
 module.exports = router;
