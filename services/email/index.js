e63e17c39f688bbf819423a22b0541f91b733029
vdjaramillo <33434357+vdjaramillo@users.noreply.github.com>
2018-05-27 14:45:10 -0500

    Add files via upload

--- services/email/index.js
+++ services/email/index.js
@@ -3,14 +3,14 @@ const express = require('express');
 const read = require('./read');
 const readOne = require('./readOne');
 const send = require('./send');
-const sent = require('./sent');
+const sended = require('./sent');
+const updateState = require('./updateState');
 
 const router = express.Router();
 
-router.get('/read', read);
-router.get('/sent',sent);
-router.post('/send', send);
-router.get('/:emailId', readOne);
-
+router.get('/read/:id', read);
+router.post('/send/', send);
+router.get('/sent/:id',sent);
+router.put('/updateState/:emailId',updateState);
 
 module.exports = router;
--- services/email/sent.js
+++ services/email/sent.js
@@ -1,10 +1,7 @@
 const { Email } = require('../../models');
 
 module.exports = (req, res) => {
-
-  if (!(typeof req.session.user === "undefined")) {
-    const { _id: id } = req.session.user;
-
+    const { id } = req.params;
     Email
       .find()
       .where("sender").equals(id)
@@ -17,12 +14,5 @@ module.exports = (req, res) => {
       .catch(error => res
         .status(500)
         .json({ errors: [error.message.toUpperCase()] }));
-  }
-  else {
-    res
-      .status(401)
-      .json({ errors: "user_not_logged" });
-  }
-
-
-};
+  };
+  
