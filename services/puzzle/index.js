6daae18d351793da04d6ee36a555019026a61a0b
Juan David Fernández Moreno <judfernandezmo@unal.edu.co>
2018-05-28 01:05:59 -0500

    puzzle back

--- services/puzzle/index.js
+++ services/puzzle/index.js
@@ -3,7 +3,9 @@ const express = require('express');
 const router = express.Router();
 
 const createPuzzle = require('./createPuzzle');
+const getPuzzles = require('./getPuzzles');
 
 router.post('/createPuzzle', createPuzzle);
+router.get('/', getPuzzles);
 
 module.exports = router;
