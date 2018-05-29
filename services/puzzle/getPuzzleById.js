51c86a2c6d42dce1551c30e354f3b3f5e70db88d
santiquiroz <squirozu@unal.edu.co>
2018-05-28 21:29:09 -0500

    final commit
    estamos melos fifi fi fi

---
+++ services/puzzle/getPuzzleById.js
@@ -0,0 +1,39 @@
+const { Puzzle } = require('../../models');
+
+module.exports = (req, res) => {
+    const { id } = req.params;
+    //const { id: userId } = req.user;
+
+    Puzzle
+        .findById(id)
+        .then((data) => {
+            if (!data) {
+                res
+                    .status(404)
+                    .json({
+                        errors: ['PUZZLE_NOT_FOUND'],
+                    });
+                return;
+            }
+            /*if ((data.sender !== userId) || (!data.receiver.find(r => r === userId))) {
+              res
+                .status(401)
+                .json({
+                  errors: ['UNAUTHORIZED'],
+                });
+
+              return;
+            }*/
+            const puzzle = data.toObject();
+            res
+                .status(200)
+                .json(project);
+        })
+        .catch((error) => {
+            res
+                .status(500)
+                .json({
+                    error: error.toString(),
+                });
+        });
+};
