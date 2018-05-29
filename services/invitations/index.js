fb2ec356476070575388b5c87ddace0fa13b3c7b
dacalles <dacalles@unal.edu.co>
2018-05-29 18:33:21 -0500

    ChooseProject back and PUT in invitations

--- services/invitations/index.js
+++ services/invitations/index.js
@@ -4,9 +4,13 @@ const router = express.Router();
 
 const getinvitations = require('./getinvitations');
 const createinvitations = require('./createinvitations');
+const getinvitationById = require('./getinvitationById');
+const updateinvitations = require('./updateinvitations');
 
 
 router.get('/', getinvitations);
 router.post('/', createinvitations);
+router.get('/', getinvitationById);
+router.put('/:invitationsId', updateinvitations);
 
 module.exports = router;
---
+++ services/invitations/updateinvitations.js
@@ -0,0 +1,33 @@
+const { invitations } = require('../../models');
+
+module.exports = (req, res) => {
+  const { invitationsId } = req.params;
+  // const { id: sender } = req.user;
+  // TODO check whether client information is sanitize;
+  // Check that receivers are allowed;
+
+    invitations
+    .findByIdAndUpdate(invitationsId, req.body)
+    .then((data) => {
+      if (!data) {
+        res
+          .status(404)
+          .json({
+            errors: ['INVITATION_NOT_FOUND'],
+          });
+        return;
+      }
+      res
+        .status(200)
+        .json({
+          result: 'success',
+        });
+    })
+    .catch((error) => {
+      res
+        .status(500)
+        .json({
+          result: error && error.message ? error.message : error.toString(),
+        });
+    });
+};
