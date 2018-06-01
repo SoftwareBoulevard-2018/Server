16557c5cc0fcba067331c11e56db70628ebcce4a
vdjaramillo <33434357+vdjaramillo@users.noreply.github.com>
2018-06-01 12:54:42 -0500

    Update index.js

--- index.js
+++ index.js
@@ -30,14 +30,6 @@ const connection = require('./models/connection');
 app.use(express.json());
 app.use(cors());
 app.use(expressValidator());
-//session and validations aren't yet fully implemented
-/*app.use(session({
-  secret: 'anything',
-  resave: false,
-  saveUninitialized: true,
-  store: new MongoStore({ mongooseConnection: connection }),
-}));*/
-
 // Mocking user data;
 app.use((req, res, next) => {
   req.user = { id: 'test_user' };
@@ -50,17 +42,6 @@ app.get('/', (req, res) => {
     app: 'Software Boulevard',
   });
 });
-
-/* // Verifies if user is logged in for /users services
-app.use('/users', (req, res, next) => {
-  if (!req.session.user) {
-    return res.status(401)
-      .json({ errors: ['UNAUTHORIZED'] })
-      .send();
-  }
-
-  return next();
-}); */
 
 app.use('/emails', emailRoutes);
 app.use('/users', userRoutes);
@@ -78,7 +59,7 @@ app.use('/invitations', invitationsRoutes);
 app.use('/chooseproject', chooseprojectRoutes);
 app.use('/trainingAttempts', trainingAttemptRoutes);
 app.use('/developingAttempts', developingAttemptRoutes);
-app.use('/puzzleImages', express.static(__dirname + '/puzzleImages'));
+app.use('/static/puzzleImages', express.static(__dirname + '/static/puzzleImages'));
 app.use('/static/company_img', express.static(__dirname + '/static/company_img'));
 app.use('/estimations', estimationRoutes);
 app.use('/certification', certificationRoutes);
