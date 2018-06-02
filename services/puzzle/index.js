b2fdee99ccf7a35a5eb5b3e5fd5b66ca4551ec8a
victorcatn <vdcatanoc@unal.edu.co>
2018-06-02 11:42:53 -0500

    upload puzzle from android

--- services/puzzle/index.js
+++ services/puzzle/index.js
@@ -3,11 +3,16 @@ const express = require('express');
 const router = express.Router();
 
 const createPuzzle = require('./createPuzzle');
+const createPuzzle = require('./createPuzzleAndroid');
 const getPuzzles = require('./getPuzzles');
 const getPuzzleById = require('./getPuzzleById');
+const getPuzzleById = require('./getPuzzleById');
+const uploadImage = require('./uploadImage');
 
 router.post('/createPuzzle', createPuzzle);
+router.post('/createPuzzleAndroid', createPuzzleAndroid);
 router.get('/getPuzzleById/:id', getPuzzleById);
 router.get('/', getPuzzles);
+router.use('/uploadImage', uploadImage);
 
 module.exports = router;
